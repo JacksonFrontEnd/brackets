b4b30ff0b5a99ce3cee835cec855cb47224e184d
JacksonFrontEnd <evgenij_poposemov@mail.ru>
2021-02-26 22:08:13 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split('');	
+	let openPos;   
+
+	for (let i = 0; i < arr.length; i++) {
+
+		 bracketsConfig.forEach((item, index) => {
+			  if (item[0] === arr[i]) {
+					openPos = index;                             
+			  }            
+		 }); 
+
+		 if (!openPos && openPos != 0) {
+			  return false;
+		 }
+
+		 const closePos = arr.indexOf(bracketsConfig[openPos][1], i + 1);
+		 if (closePos === -1) {
+			  return false;
+		 }
+
+		 if (closePos === i + 1) {
+			  arr.splice(i, 2);
+			  
+			  return check(arr.join(''), bracketsConfig);            
+		 }
+	}       
+	
+	return arr.length === 0;
 }
